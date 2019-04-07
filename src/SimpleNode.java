c84f097061a3575ac0d4469a96093ffedbe95a32
glhrmv <gvmgui@gmail.com>
2019-04-07 13:35:21 +0100

    Better syntax tree indent

--- src/SimpleNode.java
+++ src/SimpleNode.java
@@ -78,7 +78,7 @@ class SimpleNode implements Node {
       for (int i = 0; i < children.length; ++i) {
         SimpleNode n = (SimpleNode)children[i];
         if (n != null) {
-          n.dump(prefix + "\t");
+          n.dump(prefix + "  ");
         }
       }
     }
